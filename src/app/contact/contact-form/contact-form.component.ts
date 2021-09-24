16ab0a0d0115f33bb1bfc0ccacefdf9fa79a02e3
Vishnu M Kurup <vishnujishnu077@gmail.com>
2021-09-24 11:54:09 +0700

    feat(core): add contact form

--- src/app/contact/contact-form/contact-form.component.ts
+++ src/app/contact/contact-form/contact-form.component.ts
@@ -6,7 +6,6 @@ import {
   FormGroup,
   Validators
 } from '@angular/forms';
-
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
@@ -17,16 +16,7 @@ export class ContactFormComponent implements OnInit {
   formSubmitted: boolean = false;
   mask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
 
-
   constructor(private fb: FormBuilder) {
-    // this.contactForm = this.fb.group({
-    //   fname: ['', [Validators.required]],
-    //   lname: [''],
-    //   email: ['', [Validators.required, Validators.email]],
-    //   phone: ['']
-    // });
-
-
   }
 
   ngOnInit() {
@@ -43,35 +33,24 @@ export class ContactFormComponent implements OnInit {
       'email': ['', [Validators.required, Validators.email]],
       'phone': ['']
     });
-    // return new FormGroup({
-    //   fname: new FormControl('', Validators.required),
-    //   lname: new FormControl(''),
-    //   email: new FormControl('', [Validators.required, Validators.email]),
-    //   phone: new FormControl('')
-    // });
-
   }
+
   get contactFormGroups() {
     return this.contactForm.get('contacts') as FormArray
   }
+
   addContact() {
     const contacts = this.contactForm.get('contacts') as FormArray;
     contacts.push(this.createContactFormGroup());
   }
+
   onSubmit() {
     this.formSubmitted = true;
     if (this.contactForm.valid) {
       console.log(this.contactForm.value);
     }
   }
-  removeContact(contactindex: any) {
-    const contacts = this.contactForm.get('contacts') as FormArray;
-    console.log(contactindex, contacts);
-    console.log('form', this.contactForm.get(['contacts', contactindex]));
-    contacts.removeAt(contactindex);
 
-
-  }
   removeTab(contact: number) {
     // let contactArrayForm=this.contactForm.get('contacts') as FormArray;
     // if(this.contactForm.get(['contacts', contact])){
